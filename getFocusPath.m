05b828aac4a88ae9355ac634be013e889f384a4e
rn1n3r <rniner@gmail.com>
2016-06-23 16:20:37 -0400

    Fix case where no RBCs are found in path

--- getFocusPath.m
+++ getFocusPath.m
@@ -4,17 +4,22 @@ function listOfMaxFM = getFocusPath (fname, var, id)
     area = getCapillaries(var);
     
     % Store min_y, max_y, fm
-    listOfMaxFM = zeros(10000, 3);
+    listOfMaxFM = zeros(100, 3);
     count = 1;
     for i = 1:10:numel(fname)
         frame = imread(fname{i});
         listROI = findRBC(frame, area, id);
-        fm = analyseRBCFocus(listROI, id);
-        [listOfMaxFM(count, 3), ind] = max(fm)
+        fm = analyseRBCFocus(listROI, frame);
         
-        listOfMaxFM(count, 1:2) = [listROI(ind, 2) (listROI(ind, 2) + listROI(ind, 4))];
-        
-        count = count + 1
+        % In the case that there are NO RBCs found in the path
+        if ~isempty(fm)
+            [listOfMaxFM(count, 3), ind] = max(fm);
+
+            listOfMaxFM(count, 1:2) = [listROI(ind, 2) (listROI(ind, 2) + listROI(ind, 4))];
+
+            count = count + 1;
+        end
+       
     end
 
 

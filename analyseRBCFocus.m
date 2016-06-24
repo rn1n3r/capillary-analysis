f4b7d8619355093a592beaa62fe46c33e6f3f451
rn1n3r <rniner@gmail.com>
2016-06-24 10:51:44 -0400

    Some tests, added more data

--- analyseRBCFocus.m
+++ analyseRBCFocus.m
@@ -2,7 +2,6 @@ function fm = analyseRBCFocus (listROI, I)
 
     fm = zeros(size(listROI, 1), 1);
     
-    edgeI = edge(I, 'canny', 0.5);
     for i = 1:size(fm, 1)
         fm(i) = fmeasure(I, 'GLVN', listROI(i, :));
         %fm(i) = sum(sum(imcrop(edgeI, listROI(i, :))));

a39eedf2f4f0764c5a880de7dc3d439d4030626b
rn1n3r <rniner@gmail.com>
2016-06-24 15:06:55 -0400

    Changed to find the maximum GLVN at each y-coordinate (if it exists)

--- getFocusPath.m
+++ getFocusPath.m
@@ -4,7 +4,13 @@ function listOfMaxFM = getFocusPath (fname, var, id)
     area = getCapillaries(var);
     
     % Store min_y, max_y, fm
-    listOfMaxFM = zeros(100, 3);
+    % listOfMaxFM = zeros(100, 3);
+    
+    % Store the focus measure and the length of the box at each
+    % y-coordinate
+    
+    listOfMaxFM = zeros(520, 2);
+    
     count = 1;
     h = waitbar(0, 'Processing, please wait');
     for i = 1:numel(fname)
@@ -14,9 +20,15 @@ function listOfMaxFM = getFocusPath (fname, var, id)
         
         % In the case that there are NO RBCs found in the path
         if ~isempty(fm)
-            [listOfMaxFM(count, 3), ind] = max(fm);
-
-            listOfMaxFM(count, 1:2) = [listROI(ind, 2) (listROI(ind, 2) + listROI(ind, 4))];
+%             [listOfMaxFM(count, 3), ind] = max(fm);
+% 
+%             listOfMaxFM(count, 1:2) = [listROI(ind, 2) (listROI(ind, 2) + listROI(ind, 4))];
+            for j = 1:numel(fm)
+                if listOfMaxFM(listROI(j, 2), 1) < fm(j)
+                    listOfMaxFM(listROI(j, 2), 1) = fm(j);
+                    listOfMaxFM(listROI(j, 2), 2) = listROI(j, 4);
+                end
+            end
 
             count = count + 1;
         end
@@ -24,7 +36,7 @@ function listOfMaxFM = getFocusPath (fname, var, id)
             waitbar(i/numel(fname));
         end
     end
-    
+    listOfMaxFM(listOfMaxFM == 0) = NaN;
     toc
     close(h)
 end

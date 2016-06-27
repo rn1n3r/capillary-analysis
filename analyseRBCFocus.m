14863ba7d8fa4db7551ddd35ad163d52caacd7d4
rn1n3r <rniner@gmail.com>
2016-06-27 15:43:06 -0400

    Collected data for glcm corr and brenner's gradient

--- analyseRBCFocus.m
+++ analyseRBCFocus.m
@@ -3,8 +3,15 @@ function fm = analyseRBCFocus (listROI, I)
     fm = zeros(size(listROI, 1), 1);
     
     for i = 1:size(fm, 1)
-        fm(i) = fmeasure(I, 'GLVN', listROI(i, :));
+        %fm(i) = fmeasure(I, 'GLVN', listROI(i, :));
         %fm(i) = sum(sum(imcrop(edgeI, listROI(i, :))));
+        
+        % Graycoprops
+%         glcm = graycomatrix(imcrop(I, listROI(i, :)));
+%         gprops = graycoprops(glcm);
+%         fm(i) = gprops.Correlation;
+
+        fm(i) = fmeasure(I, 'BREN', listROI(i, :));
     end
 
 end

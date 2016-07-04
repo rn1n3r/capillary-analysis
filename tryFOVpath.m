42b5ae9a81e796cd4b409fff753afc5bad415bd0
rn1n3r <rniner@gmail.com>
2016-07-04 16:17:10 -0400

    Parallelized everything

--- tryFOVpath.m
+++ tryFOVpath.m
@@ -1,7 +1,10 @@
+tic
 % Clean and get variance data
 clean;
 load('data/varianceData.mat');
 
+% Start parallel processing pool
+matlabpool;
 
 % FOV3
 %fname = get_fnames('C:\Users\Edward\Documents\Files\DUROP\DATA-2\20150619/442/X20-FOV3-B/');
@@ -20,10 +23,11 @@ clear fnames;
 fname = get_fnames('/Volumes/DATA-2/Captured/20150619/442/X20-FOV5-B/');
 [areas5 idList5] = getCapillaries(var5);
 fov5 = cell(size(idList5, 1), 2);
-for i = 1:size(idList5, 1)
+fov5(:, 1) = num2cell(idList5(:, 1));
+
+parfor i = 1:size(idList5, 1)
     temp = getFocusPath(fname, var5, idList5(i));
-    fov5{i, 1} = idList5(i);
-    fov5{i, 2} = temp;
+    fov5{i, 2} = {temp};
 end
 
 clear fnames;
@@ -31,9 +35,10 @@ clear fnames;
 fname = get_fnames('/Volumes/DATA-2/Captured/20150619/442/X20-FOV7-B/');
 [areas7 idList7] = getCapillaries(var7);
 fov7 = cell(size(idList7, 1), 2);
-for i = 1:size(idList7, 1)
+fov7(:, 1) = num2cell(idList7(:, 1));
+
+parfor i = 1:size(idList7, 1)
     temp = getFocusPath(fname, var7, idList7(i));
-    fov7{i, 1} = idList7(i);
     fov7{i, 2} = temp;
 end
 
@@ -42,9 +47,10 @@ clear fnames;
 fname = get_fnames('/Volumes/DATA-2/Captured/20150619/442/X20-FOV8-B/');
 [areas8 idList8] = getCapillaries(var8);
 fov8 = cell(size(idList8, 1), 2);
-for i = 1:size(idList8, 1)
+fov8(:, 1) = num2cell(idList8(:, 1));
+
+parfor i = 1:size(idList8, 1)
     temp = getFocusPath(fname, var8, idList8(i));
-    fov8{i, 1} = idList8(i);
     fov8{i, 2} = temp;
 end
 
@@ -53,9 +59,10 @@ clear fnames;
 fname = get_fnames('/Volumes/DATA-2/Captured/20150619/442/X20-FOVp2-2-B/');
 [areasp22 idListp22] = getCapillaries(varp22);
 fovp22 = cell(size(idListp22, 1), 2);
-for i = 1:size(idListp22, 1)
+fovp22(:, 1) = num2cell(idListp22(:, 1));
+
+parfor i = 1:size(idListp22, 1)
     temp = getFocusPath(fname, varp22, idListp22(i));
-    fovp22{i, 1} = idListp22(i);
     fovp22{i, 2} = temp;
 end
 
@@ -64,9 +71,13 @@ clear fnames;
 fname = get_fnames('/Volumes/DATA-2/Captured/20150619/442/X20-FOVp2-4-P/');
 [areasp24 idListp24] = getCapillaries(varp24);
 fovp24 = cell(size(idListp24, 1), 2);
-for i = 1:size(idListp24, 1)
+fovp24(:, 1) = num2cell(idListp24(:, 1));
+
+parfor i = 1:size(idListp24, 1)
     temp = getFocusPath(fname, varp24, idListp24(i));
-    fovp24{i, 1} = idListp24(i);
     fovp24{i, 2} = temp;
 end
-% % total = [total3;total5;total7;total8;totalp22;totalp24;];
+
+% Close pool
+matlabpool close;
+toc

d5d8b8beee2cbb44da401be690fded2c5e8b9a23
rn1n3r <rniner@gmail.com>
2016-07-12 11:47:39 -0400

    changed gmsh.m to use the new jpg

--- gmsh.m
+++ gmsh.m
@@ -2,34 +2,45 @@ function gmsh (I)
 
     
     % Get the capillaries
-    gray = rgb2gray(I);
-    finalImage = zeros(size(gray));
-    binaryImage = im2bw(gray);
+    g = rgb2gray(I);
+    finalImage = zeros(size(g));
+    binaryImage = im2bw(g);
     capillaries = ~binaryImage;
     
     s = regionprops(capillaries);
     
-    for i = 1:numel(s)
-        if s(i).Area > 20
-            x = uint16(s(i).Centroid(2));
-            y = uint16(s(i).Centroid(1));
-            finalImage(x-1:x+1, y-1:y+1) = 1;
-        end
-            
-            
-    end
+%     for i = 1:numel(s)
+%         if s(i).Area > 20
+%             x = uint16(s(i).Centroid(2));
+%             y = uint16(s(i).Centroid(1));
+%             finalImage(x-1:x+1, y-1:y+1) = 1;
+%         end    
+%     end
     
-%     % Attempt to get lines
-%     
-%     complement = imcomplement(gray);
-%     %complement = imfilter(complement, fspecial('gaussian'));
-%     boundaries = watershed(imhmin(complement, 20));
-%     
-    imshow(I);
+    % Attempt to get interstitial
+    green = I(:, :, 2);
+    
+    % Binary threshold
+    green(green < 153) = 0;
+    
+    % Convert to binary, use bwlabel
+    green = im2bw(green);
+    green = bwlabel(green);
+    imshow(green);
     figure;
-    imshow(finalImage);
-    figure;
-%     imshow(boundaries)
+    % Get second longest label (first will be 0)
+    uniqueLabel = unique(green)
+    valueCount = histc(green(:), uniqueLabel)
+    [~, idx] = max(valueCount)
+    valueCount(idx) = 0;
+    [~, idx] = max(valueCount)
+    label = uniqueLabel(idx);
+    
+    % Show the labelled image
+    imshow(green == label);
+    
+    
+    
   
 
 

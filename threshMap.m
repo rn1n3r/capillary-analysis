dcd0d85793b4c0d19c5fed592159461bfa66ea6f
rn1n3r <rniner@gmail.com>
2017-03-03 19:11:24 -0500

    Added ROC curve generation

---
+++ threshMap.m
@@ -0,0 +1,31 @@
+function threshmap = threshMap(fov, area, threshold, disp)
+
+    for i = 1:length(fov)
+        currentID = fov{i, 1};
+        
+        values = nanmean(fov{i, 2}, 2);
+        %stdValues = nanstd(fov{i, 2}, 0, 2);
+        %CV = nanstd(fov{i, 2}, 0, 2)./values;
+        
+       
+        
+        [y, x] = ind2sub(size(area), find(area == currentID));
+        
+        for j = min(y):max(y)
+            currentRow = find(area(j, :) == currentID);
+            if values(j) > threshold
+                area(j, currentRow) = 1;
+            else
+                area(j, currentRow) = 0;
+        end
+        
+    end
+    
+    threshmap = area;
+    
+    if disp
+        imshow(area, []);
+        colormap(jet);
+    end
+
+end

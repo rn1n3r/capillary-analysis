8446dab7edba0feed157d4ece30e1e676b240d37
rn1n3r <rniner@gmail.com>
2017-03-07 19:38:04 -0500

    Temporary change to ROCscript, need to make it so I can use any FOV more easily

--- ROCscript.m
+++ ROCscript.m
@@ -1,11 +1,12 @@
 % In focus
 
 if isunix
-    varImg = imread('/Volumes/DATA-2/Processed/20150619/X20-FOV3-B/Functional-16bitImages/X20-FOV3-B-16bit442Var.tif');
+    varImg = imread('/Volumes/DATA-2/Processed/20150619/X20-FOV5-B/Functional-16bitImages/X20-FOV5-B-16bit442Var.tif');
     %load('/Users/edward/data/BREN-testMiddle.mat');
     area = getCapillaries(varImg);
-    fov = fovData.('X20FOV3B');
-    [aroc, TPF, FPF] = generateROC(fov, area, [1100 1100 2100 600 4100 3100], '/Volumes/DATA-2/Processed/20150619/X20-FOV3-B')
+    fov = fovData.('X20FOV5B');
+    %[aroc, TPF, FPF] = generateROC(fov, area, [1100 1100 2100 600 4100 3100], '/Volumes/DATA-2/Processed/20150619/X20-FOV3-B');
+    [aroc, TPF, FPF] = generateROC(fov, area, [4600 2600 3100 600], '/Volumes/DATA-2/Processed/20150619/X20-FOV5-B');
 
 else
     varImg = imread('..\DATA-2\Processed\X20-FOV3-B\Functional-16bitImages\X20-FOV3-B-16bit442Var.tif');

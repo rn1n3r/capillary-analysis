f3af3b400a8b99aa44c8c71865d070b7ba330caf
Edward Ho <rniner@gmail.com>
2018-01-09 11:51:45 -0500

    Added comments and refactored threshMap

--- threshMap.m
+++ threshMap.m
@@ -1,26 +1,49 @@
-function threshmap = threshMap(fov, meanValues, area, threshold, disp)
+% Generate a mapping of true/false (focus/not focus) on the capillaries of
+% an ids based on a given threshold value
 
-    for i = 1:length(fov)
-        currentID = fov{i, 1};
+% ids = cell with capillary ids as column 1 and
+%                 focus data as column 2
+
+% capMask is the capillary mask of the FOV (from getCapillaries)
+
+% threshold is the thresold value
+
+% disp is a boolean variable indicating whether the resulting threshold map
+% should be displayed
+
+% meanValues is an array with time-averaged focus data for each capillary
+% on each row. If an empty array is passed, this is calculated in the
+% function
+function threshmap = threshMap(ids, capMask, threshold, disp, meanValues)
+
+    for i = 1:length(ids)
+        currentID = ids{i, 1};
+        
+        if isempty(meanValues)
+            meanValues = zeros(520, length(ids));
+            for j = 1:length(ids)
+               meanValues(:, j) = nanmean(ids{j,2},2); 
+            end
+        end
         
         values = meanValues(:, i);
         
-        [y, x] = ind2sub(size(area), find(area == currentID));
+        [y, x] = ind2sub(size(capMask), find(capMask == currentID));
         
         for j = min(y):max(y)
-            currentRow = find(area(j, :) == currentID);
+            currentRow = find(capMask(j, :) == currentID);
             if values(j) > threshold
-                area(j, currentRow) = 1;
+                capMask(j, currentRow) = 1;
             else
-                area(j, currentRow) = 0;
+                capMask(j, currentRow) = 0;
             end
         end
     end
     
-    threshmap = area;
+    threshmap = capMask;
    
     if disp
-        imshow(area);
+        imshow(capMask, []);
         
     end
 
